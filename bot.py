6b9788e14cd32724dff545507547703a69b396fa
MAHI® <155721977+Nrgodara@users.noreply.github.com>
2024-12-04 23:00:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,11 +51,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/susanta_bhandarii for support")
-            sys.exit()
+            self.LOGGER(__name__).info("\nBot Stopped.")
+           # sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/waspros")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \n")
         self.LOGGER(__name__).info(f""" \n\n       
 ░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
 ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝╚════██║
